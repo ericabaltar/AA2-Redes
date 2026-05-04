012f8764ce6e826998e9822e49c6a6051d81cd90
erica <erica.baltar@enti.cat>
2026-05-04 10:31:58 +0200

    Movement animation

--- Tutorial SFML/Tutorial SFML/Tutorial SFML/Source.cpp
+++ Tutorial SFML/Tutorial SFML/Tutorial SFML/Source.cpp
@@ -1,14 +1,36 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
 #include <cmath>
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Coyote Time and Jump Buffering");
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML Test");
 
-    sf::RectangleShape square({ 50.f, 50.f });
-    square.setFillColor(sf::Color::Green);
-    square.setPosition({ 375.f, 300.f });
+    sf::Texture duckTexture;
+    if (!duckTexture.loadFromFile("assets/grey_duck_movement.png"))
+        return -1;
+
+    const int frameWidth = 15;
+    const int frameHeight = 25;
+    const float scale = 4.f;
+
+    sf::Sprite player(duckTexture);
+
+    std::vector<sf::IntRect> walkFrames =
+    {
+        sf::IntRect({ 0, 0 },   { frameWidth, frameHeight }),
+        sf::IntRect({ 32, 0 },  { frameWidth, frameHeight }),
+        sf::IntRect({ 64, 0 },  { frameWidth, frameHeight }),
+        sf::IntRect({ 96, 0 },  { frameWidth, frameHeight }),
+        sf::IntRect({ 128, 0 }, { frameWidth, frameHeight }),
+        sf::IntRect({ 160, 0 }, { frameWidth, frameHeight })
+    };
+
+    player.setTextureRect(walkFrames[0]);
+    player.setOrigin({ frameWidth / 2.f, frameHeight / 2.f });
+    player.setPosition({ 400.f, 300.f });
+    player.setScale({ scale, scale });
 
     sf::RectangleShape ground({ 800.f, 50.f });
     ground.setFillColor(sf::Color(100, 100, 100));
@@ -30,6 +52,12 @@ int main()
     float jumpBufferTimer = 0.f;
 
     bool isOnGround = false;
+
+    float animationTimer = 0.f;
+    const float animationSpeed = 0.08f;
+    int currentFrame = 0;
+
+    bool facingRight = true;
 
     sf::Clock clock;
 
@@ -58,15 +86,11 @@ int main()
             inputX += 1.f;
 
         velocity.x += inputX * acceleration * dt;
-
-        // Friction
         velocity.x *= 1.f / (1.f + friction * dt);
 
-        // Clamp speed
         if (std::abs(velocity.x) > maxSpeed)
             velocity.x = (velocity.x > 0.f ? 1.f : -1.f) * maxSpeed;
 
-        // Timers
         if (isOnGround)
             coyoteTimer = coyoteTime;
         else
@@ -77,7 +101,6 @@ int main()
         if (jumpBufferTimer > 0.f && coyoteTimer > 0.f)
         {
             velocity.y = jumpForce;
-
             jumpBufferTimer = 0.f;
             coyoteTimer = 0.f;
             isOnGround = false;
@@ -85,16 +108,18 @@ int main()
 
         velocity.y += gravity * dt;
 
-        square.move(velocity * dt);
+        player.move(velocity * dt);
 
-        sf::Vector2f pos = square.getPosition();
-        sf::Vector2f size = square.getSize();
+        sf::Vector2f pos = player.getPosition();
+
+        float duckWidth = frameWidth * scale;
+        float duckHeight = frameHeight * scale;
 
         float groundY = ground.getPosition().y;
 
-        if (pos.y + size.y >= groundY)
+        if (pos.y + duckHeight / 2.f >= groundY)
         {
-            square.setPosition({ pos.x, groundY - size.y });
+            player.setPosition({ pos.x, groundY - duckHeight / 2.f });
             velocity.y = 0.f;
             isOnGround = true;
         }
@@ -103,24 +128,52 @@ int main()
             isOnGround = false;
         }
 
-        // Screen walls
-        pos = square.getPosition();
+        pos = player.getPosition();
 
-        if (pos.x < 0.f)
+        if (pos.x - duckWidth / 2.f < 0.f)
         {
-            square.setPosition({ 0.f, pos.y });
+            player.setPosition({ duckWidth / 2.f, pos.y });
             velocity.x = 0.f;
         }
 
-        if (pos.x + size.x > 800.f)
+        if (pos.x + duckWidth / 2.f > 800.f)
         {
-            square.setPosition({ 800.f - size.x, pos.y });
+            player.setPosition({ 800.f - duckWidth / 2.f, pos.y });
             velocity.x = 0.f;
         }
 
-        window.clear();
+        bool isMoving = std::abs(velocity.x) > 20.f;
+
+        if (velocity.x > 5.f)
+            facingRight = true;
+        else if (velocity.x < -5.f)
+            facingRight = false;
+
+        if (isMoving)
+        {
+            animationTimer += dt;
+
+            if (animationTimer >= animationSpeed)
+            {
+                animationTimer = 0.f;
+                currentFrame = (currentFrame + 1) % walkFrames.size();
+                player.setTextureRect(walkFrames[currentFrame]);
+            }
+        }
+        else
+        {
+            currentFrame = 0;
+            player.setTextureRect(walkFrames[0]);
+        }
+
+        if (facingRight)
+            player.setScale({ scale, scale });
+        else
+            player.setScale({ -scale, scale });
+
+        window.clear(sf::Color::Black);
         window.draw(ground);
-        window.draw(square);
+        window.draw(player);
         window.display();
     }
 
