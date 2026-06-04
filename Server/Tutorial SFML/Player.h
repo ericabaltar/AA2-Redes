b73992e4aab181a97cf60cd777e6ff43e1c8f583
erica <erica.baltar@enti.cat>
2026-06-04 10:34:16 +0200

    basic functionality game rooms

--- Server/Tutorial SFML/Player.h
+++ Server/Tutorial SFML/Player.h
@@ -1,16 +1,24 @@
 #pragma once
 #include <SFML/Network.hpp>
+#include <optional>
+#include <string>
 
 class Player
 {
 public:
 	Player()
-		: client(nullptr), name(""), points(0) {}
+		: client(nullptr), name(""), points(0), udpPort(0) {
+	}
 
 	Player(sf::TcpSocket* playerClient, std::string playerName, int playerPoints)
-		: client(playerClient), name(playerName), points(playerPoints) { }
+		: client(playerClient), name(playerName), points(playerPoints), udpPort(0) {
+	}
 
 	sf::TcpSocket* client;
 	std::string name;
 	int points;
+
+	// Endpoint UDP del jugador. Se actualiza cuando el servidor recibe un paquete UDP suyo.
+	std::optional<sf::IpAddress> udpIp;
+	unsigned short udpPort;
 };
